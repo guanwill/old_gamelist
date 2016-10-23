be21993ea054f0f935fa117820ec2609aea62781
Will G <w.guan@hotmail.com>
2016-10-23 13:41:14 +0000

    made the whole website responsive and scales to diff screen sizes

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,8 +1,8 @@
 class Game < ActiveRecord::Base
 
   GENRES = ['Action', 'Adventure', 'RPG', 'Simulation', 'Strategy', 'Sports', 'FPS', 'Novel', 'Others']
-  PLATFORMS = ['3DS', 'WII U', 'NX', 'PC', 'Playstation', 'XBOX', 'PS Vita', 'Mobile', 'Others']
-  PROGRESS = [0, 25, 50, 75, 100, 200]
+  PLATFORMS = ['3DS', 'WII U', 'Switch', 'PC', 'PS4', 'XBOX', 'PS Vita', 'Mobile', 'Others']
+  PROGRESS = [0, 10, 25, 50, 75, 100, 200]
 
   validates :title, presence: true, :uniqueness => true
   # validates :platform, presence: true
