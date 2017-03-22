1e5a7e16f4e278f6f2c234717adb42c21a4e0fa7
guanwill <w.guan@hotmail.com>
2017-03-22 17:20:14 +1100

    styled game search results add game form

--- app/assets/javascripts/components/home/result_items.jsx
+++ app/assets/javascripts/components/home/result_items.jsx
@@ -1,5 +1,5 @@
 var ResultItems = React.createClass({
-  
+
   render: function(){
 
     var game = this.props.gameResult;
@@ -7,19 +7,25 @@ var ResultItems = React.createClass({
     var data_target_classname = ".gameid" + game.id //for if you want to trigger addGame form in a modal
 
     return(
-      <div className="game_record_container row row-eq-height">
+      <div className="game_record_container">
+        <div className="row row-eq-height">
 
-        <div className='col-md-2 col-sm-2 image-col'>
-          <img className="api-image=" src={game.image.icon_url}/>
+          <div className='col-md-2 col-sm-2 image-col'>
+            <img className="api-image=" src={game.image.icon_url}/>
+          </div>
+
+          <div className='col-md-10 col-sm-10 para-col'>
+            <h4>{game.name}</h4>
+            {game.deck}
+
+            <a className="game_status_title_last" data-toggle="collapse" data-target={data_target_name}>Add</a>
+          </div>
         </div>
 
-        <div className='col-md-10 col-sm-10 para-col'>
-          <h4>{game.name}</h4>
-          {game.deck}
-
-          <a className="game_status_title_last" data-toggle="collapse" data-target={data_target_name}>Add</a>
-          <AddGameForm2 gameResult={game} />
-
+        <div className="row">
+          <div className='col-md-12 col-sm-12'>
+            <AddGameForm2 gameResult={game} />
+          </div>
         </div>
 
       </div>
--- config/routes.rb
+++ config/routes.rb
@@ -1,28 +1,27 @@
 Rails.application.routes.draw do
 
-resources :games do
-  get :autocomplete_gamesapi_name, :on => :collection
-end
-
-namespace :api do
-  resources :gamesapi, defaults: {format: :json} do
+  resources :games do
+    get :autocomplete_gamesapi_name, :on => :collection
   end
-end
 
-post '/rate' => 'rater#create', :as => 'rate'
-devise_for :users
-# devise_for :users, :controllers => {:registrations => "my_devise/registrations"}
-root to: "home#homeindex"
-# root to: "games#index"
+  namespace :api do
+    resources :gamesapi, defaults: {format: :json} do
+    end
+  end
 
-resources :games
-get '/plan' => 'games#plan'
-get '/completed' => 'games#completed'
-get '/about' => 'about#about'
-post '/search' => 'about#search'
+  post '/rate' => 'rater#create', :as => 'rate'
+  devise_for :users
+  # devise_for :users, :controllers => {:registrations => "my_devise/registrations"}
+  root to: "home#homeindex"
 
-# Views that use React Components
-get '/games_index' => 'games#newindex'
-get '/home' => 'home#homeindex'
+  resources :games
+  # get '/plan' => 'games#plan'
+  # get '/completed' => 'games#completed'
+  # get '/about' => 'about#about'
+  # post '/search' => 'about#search'
+
+  # Views that use React Components
+  get '/games_index' => 'games#newindex'
+  get '/home' => 'home#homeindex'
 
 end
