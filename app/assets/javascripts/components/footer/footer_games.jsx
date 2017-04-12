3078cc6f307528e8df6463bfa73d3058d8d18f7e
guanwill <w.guan@hotmail.com>
2017-04-12 19:16:31 +1000

    added check if game image exists from api

--- app/assets/javascripts/components/footer/footer_games.jsx
+++ app/assets/javascripts/components/footer/footer_games.jsx
@@ -10,10 +10,14 @@ var FooterGames = React.createClass({
       var release_date = "TBA"
     }
 
+    if (game.image != null){
+      var game_image_url = game.image.small_url
+    }
+
     return(
       // <div className="col-md-2 col-sm-2">
       <li>
-        <a target="_blank" href={game.site_detail_url}><img className="api-image=" src={game.image.small_url}/></a>
+        <a target="_blank" href={game.site_detail_url}><img className="api-image=" src={game_image_url}/></a>
         <p><a target="_blank" href={game.site_detail_url}>{game.name}</a></p>
         <p> {release_date} </p>
       </li>
